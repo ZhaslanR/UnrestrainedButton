0b1e0d5e89f309d79345cc2ab1e8027fb0cda36e
Жаслан Рыспеков <zhaslan_12@bk.ru>
2018-01-26 21:41:44 +0600

    Добавьте файлы проекта.

---
+++ UnrestrainedButtonWPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnrestrainedButtonWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(button.VerticalAlignment == VerticalAlignment.Bottom)
+                button.VerticalAlignment = VerticalAlignment.Bottom;
+            else
+                button.VerticalAlignment = VerticalAlignment.Top;
+
+            if (button.HorizontalAlignment == HorizontalAlignment.Right)
+                button.HorizontalAlignment = HorizontalAlignment.Left;
+            else
+                button.HorizontalAlignment = HorizontalAlignment.Right;
+        }
+    }
+}
